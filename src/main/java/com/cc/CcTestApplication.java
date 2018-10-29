45bd0a77cfe9f873baa20aaf9e272b1800ffa20d
chang_chao@founder.com.cn <Founder123>
2018-10-29 16:38:58 +0800

    测试项目（springboot）

---
+++ src/main/java/com/cc/CcTestApplication.java
@@ -0,0 +1,12 @@
+package com.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcTestApplication.class, args);
+	}
+}
